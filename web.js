a0e9f019a475460a408f9ec3692159934f4d9b2c
Zeljko Tepsic <ztepsic@gmail.com>
2013-07-07 22:48:29 +0200

    Reading content from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var text = fs.readFileSync('./index.html').toString();
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
